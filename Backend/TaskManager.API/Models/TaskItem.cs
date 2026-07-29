b4dec716c72358335d6ef4261936455bdb0c2a87
Layomx <drakeadamslink@gmail.com>
2026-07-29 17:17:31 -0400

    fix: missing brackets

--- Backend/TaskManager.API/Models/TaskItem.cs
+++ Backend/TaskManager.API/Models/TaskItem.cs
@@ -1,15 +1,17 @@
 using System;
 
 namespace TaskManager.Api.Models
-
-public class TaskItem
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public string Title { get; set; } = string.Empty;
-    public string? Description { get; set; } = false;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime? UpdatedAt { get; set; }
-    
-    public Guid UserId { get; set; }    // foreign key
-    public User? User { get; set; }     // Referencia de navegacion
+
+    public class TaskItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        public Guid UserId { get; set; }    // foreign key
+        public User? User { get; set; }     // Referencia de navegacion
+    }
 }
