4fed87eb46f11e06d086b6a11ec617a573b1441b
Layomx <drakeadamslink@gmail.com>
2026-07-29 19:27:41 -0400

    feat: main program updates

--- Backend/TaskManager.API/Program.cs
+++ Backend/TaskManager.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TaskManager.API.Data;
 using TaskManager.API.Middleware;
+using TaskManager.API.Repositories;
 using TaskManager.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,11 +14,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Registro de la capa de Autenticacion
 builder.Services.AddScoped<IAuthService, AuthService>();
+
+// Registro de la capa de Tareas y servicios
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<ITaskService, TaskService>();
 
 var app = builder.Build();
 
-// Activacion del MIddleware global de errores
+// Activacion del Middleware global de errores
 app.UseMiddleware<ExceptionMiddleware>();
 
 // Pipeline de peticiones HTTP
