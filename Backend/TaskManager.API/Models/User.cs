38c6ce625f63c832ed804ec2c7a0c75618f59022
Layomx <drakeadamslink@gmail.com>
2026-07-29 17:04:38 -0400

    feat: user model for db

---
+++ Backend/TaskManager.API/Models/User.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace TaskManager.API.Models
+{
+    // Tabla de usuarios en el sistema
+    public class User
+    {
+        // Guid como modelo autoincremental
+        public Guid Id { get; set; } = Guid.NewGUid();
+        public string Username { get; set; } = string.Empty;
+        // Contrasena en hash
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTIme CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Administrador de tareas sencillo
+
+Este repositorio sirve como un administrador de tareas sencillo escrito con frontend y backend relativamente basico.
