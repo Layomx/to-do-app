af1ca856076a5eb9f4a2fe9e34edcb36e92b4388
Layomx <drakeadamslink@gmail.com>
2026-07-29 17:33:01 -0400

    fix: brackets fix and context for database

--- Backend/TaskManager.API/Data/AppDbContext.cs
+++ Backend/TaskManager.API/Data/AppDbContext.cs
@@ -3,4 +3,33 @@ using TaskManager.API.Models;
 
 namespace TaskManager.API.Data;
 
-public class AppDbContext : DbContext:
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasIndex(u => u.Username).IsUnique(); // No se pueden repetir usuarios
+            entity.Property(u => u.Username).IsRequired().HasMaxLength(50);
+            entity.Property(u => u.PasswordHash).IsRequired();
+        });
+
+        modelBuilder.Entity<TaskItem>(entity =>
+        {
+            entity.Property(t => t.Tile).IsRequired().HasMaxLength(200);
+            entity.HasOne(t => t.User)
+                  .WithMany(u => u.Tasks)
+                  .HasForeignKey(t => t.UserId)
+                  .OnDelete(DeleteBehavior.Cascada); // borrar el usuario borra sus tareas
+
+            entity.HasINdex(t => new { t.UserId, t.IsCompleted });
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
